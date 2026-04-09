6faf26ebc4aa29858b5e4cdea8323f4de2a653ba
Sarita <saritasilva368@gmail.com>
2026-04-09 23:09:38 +0100

    Install React and Bootstrap

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+          <h1 className="text-center text-info mt-5">Dictionary</h1>
+        </header>
+        <main>
+          <p>Dictionary project</p>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
