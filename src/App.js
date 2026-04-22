a106be985f71c09ec8dd24d8c02f7a2bf238a999
Sarita <saritasilva368@gmail.com>
2026-04-22 15:24:15 +0100

    added footer component

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
           <h1 className="text-center text-info mt-5">Dictionary</h1>
         </header>
         <main>
           <p>Dictionary project</p>
         </main>
+        <footer className="App-footer">
+          <p className="text-center text-muted">Coded by Sara</p>
+        </footer>
       </div>
     </div>
   );
