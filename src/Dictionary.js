9506d70f2aa95ac731dd4716bd94d377023f9bb7
Sarita <saritasilva368@gmail.com>
2026-04-24 14:22:51 +0100

    installed axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,35 +5,39 @@ import "./Dictionary.css";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
-
-function handleResponse(response) {
+  function handleResponse(response) {
     console.log(response.data);
-}
-  
-    
-   function search(event) {
+  }
+
+  function search(event) {
     event.preventDefault();
+
+    alert(`Searching for the word: ${keyword}`);
+
     let apiKey = "3dfefaofbc33edd4c68fae7t3005c4aa";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
     axios.get(apiUrl).then(handleResponse);
-   }
+  }
 
-   function handleKeywordChange(event) {
+  function handleKeywordChange(event) {
     setKeyword(event.target.value.toLowerCase());
-   }
+  }
 
-  return (
+return (
     <div className="Dictionary">
-      <form onSubmit={search}>
-        <input
-          type="search"
-          autoFocus={true}
-          className="form-control"
-          placeholder="Search for a word..."
-          value={keyword}
-          onChange={handleKeywordChange}
-        />
-      </form>
+      <section>
+        <form onSubmit={search}>
+          <input
+            type="search"
+            autoFocus={true}
+            className="form-control"
+            placeholder="Search for a word..."
+            value={keyword}
+            onChange={handleKeywordChange}
+          />
+        </form>
+      </section>
     </div>
   );
 }
